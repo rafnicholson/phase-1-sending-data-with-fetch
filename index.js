7e26682d6fa249b929531643df957517572b2f12
Ralph Nicholson <nicholson.re@gmail.com>
2023-04-30 09:36:14 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(res => res.json())
+    .then(data => {
+        const body = document.querySelector('body');
+        const div = document.createElement('div');
+        div.textContent = data.id;
+        body.appendChild(div);
+    })
+    .catch(error => {
+        const body = document.querySelector('body');
+        const div = document.createElement('div');
+        div.textContent = error.message;
+        body.appendChild(div);
+    });
+}
